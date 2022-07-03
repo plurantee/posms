2572d170462097b9a909b0bc42a01e7c8aaef406
Florante Rapio <rapioflorante1@gmail.com>
2022-07-03 14:14:10 +0800

    Initial version of posms generated by generator-jhipster@7.8.1

---
+++ src/main/java/com/flogramming/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.flogramming.repository;
+
+import com.flogramming.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/test/java/com/flogramming/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.flogramming;
+
+import com.flogramming.PosmsApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = PosmsApp.class)
+public @interface IntegrationTest {
+}
